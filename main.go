20945b774921764e2c1d3e76cb6ebc7a087b0e90
Marton Bocsi <mvbocsi@gmail.com>
2024-06-10 00:15:27 -0500

    Added router and handler.

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"path"
+
+	"github.com/mbocsi/file-based-router/router"
+)
 
 func main() {
-	fmt.Println("Welcome to the mbocsi router!")
+	rootRoute := flag.String("root", "routes", "The root route directory")
+	port := flag.Int("port", 8080, "Listening port")
+	flag.Parse()
+	root := path.Clean(*rootRoute)
+	addr := fmt.Sprintf(":%v", *port)
+
+	handler := router.NewFBHandler(root)
+	router := router.NewFBRouter(addr, handler)
+	fmt.Printf("Running on %v\nServing files from %v\n", addr, root)
+
+	err := router.Run()
+	if err != nil {
+		fmt.Println("Error when running router")
+	}
 }
